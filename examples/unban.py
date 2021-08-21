d3b8498ff02ec6fe9c52e113e0d5168910b48fcb
Nebulizer1213 <fixingg@gmail.com>
2021-08-21 19:50:32 -0400

    Update unban.py

--- examples/unban.py
+++ examples/unban.py
@@ -7,6 +7,6 @@ TOKEN = "Your Token"
 @client.command(name="unban")
 async def unban(ctx, member):
     # This can take ID or name#discriminator
-    # It will through an error you input something like nkjahsdklj
+    # It will throw an error you input something like nkjahsdklj
     # It's your job to make sure they input a member dpys can use
     await dpys.admin.unban(ctx, member)
