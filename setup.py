af4bed6351f447b2fe0b982ee8e6712c2909e74b
Nebulizer1213 <fixingg@gmail.com>
2021-08-24 01:30:07 -0400

    update

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ REQUIREMENTS = [
     "python-multipart",
 ]
 DOCS = "https://jgltechnologies.com/dpys"
-VERSION = "4.4.1"
+VERSION = "4.4.2"
 
 classifiers = [
     "Development Status :: 4 - Beta",
