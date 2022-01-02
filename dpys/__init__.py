8404db13351a08e73cdd03da72a337dc793a6504
Nebula <79063894+Nebulizer1213@users.noreply.github.com>
2022-01-01 21:18:18 -0500

    Update __init__.py

--- dpys/__init__.py
+++ dpys/__init__.py
@@ -5,7 +5,7 @@
 # https://discord.gg/TUUbzTa3B7
 
 """
-Copyright (c) 2021 George Luca
+Copyright (c) 2021 JGL Technologies
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
